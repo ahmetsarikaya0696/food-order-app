22427cf41b5a6e5ce945e70661dae24230f1f8b0
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-24 16:28:37 +0300

    context ile yemeklerin tümü listelendi

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 
-const MealItem = () => {
+const MealItem = (props) => {
   return (
     <li className={styles.meal}>
       <div>
-        <h3>H3 Element</h3>
-        <div className={styles.description}>Description</div>
-        <div className={styles.price}>22.99</div>
+        <h3>{props.name}</h3>
+        <div className={styles.description}>{props.description}</div>
+        <div className={styles.price}>{props.price} $</div>
       </div>
       <MealItemForm />
     </li>
--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card/Card";
+import AuthContext from "../../context/auth-context";
 
 const MealList = () => {
+  const ctx = useContext(AuthContext);
+
   return (
     <Card>
       <ul>
-        <MealItem />
+        {ctx.meals.map((meal) => (
+          <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+          />
+        ))}
       </ul>
     </Card>
   );
